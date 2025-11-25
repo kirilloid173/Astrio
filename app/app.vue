dcc6fb72e7d3fde00037a33b3b4b2e1fca6c60c6
Amogus <empty>
2025-11-25 11:04:53 +0400

    0.1-start

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+      
+    </div>
+</template>
+
+<style lang="css">
+.container {
+    width: 1440px;
+}
+</style>
---
+++ app/components/HeroSection/hero.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="hero-section"></div>
+</template>
+
+<style>
+.hero-section {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+</style>
