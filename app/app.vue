e243b6edf4b2473302c4134834cfcfd0abdcfa08
Amogus <empty>
2025-11-25 11:20:30 +0400

    0.2-hero_section_ready

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,16 @@
+<script setup lang="ts">
+import HeroSection from './components/HeroSection/HeroSection.vue';
+</script>
+
 <template>
     <div class="container">
-      
+        <HeroSection />
     </div>
 </template>
 
 <style lang="css">
 .container {
     width: 1440px;
+    margin: 0 auto;
 }
 </style>
---
+++ app/components/HeroSection/HeroSection.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="hero-section">
+        <img src="/images/logo.png" alt="logo" />
+        <img
+            class="hero-section__cart"
+            src="/images/cart_logo.svg"
+            alt="cart"
+        />
+    </div>
+</template>
+
+<style>
+.hero-section {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 2px solid rgb(42, 40, 40);
+    padding-bottom: 10px;
+}
+
+.hero-section__cart {
+    width: 60px;
+    height: 60px;
+}
+</style>
