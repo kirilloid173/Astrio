9ec382edfeffacd0d7fa4ed9d9be5bd08780f0a3
Amogus <empty>
2025-11-25 13:16:43 +0400

    0.4-connect_custom_fonts

--- app/app.vue
+++ app/app.vue
@@ -11,10 +11,16 @@ import Catalog from './components/catalog/Catalog.vue';
 </template>
 
 <style lang="css">
+@font-face {
+    font-family: 'Inter';
+    src: url('/assets/fonts/Inter.ttf');
+}
+
 * {
     padding: 0;
     margin: 0;
     box-sizing: content-box;
+    font-family: 'Inter', Arial, Helvetica, sans-serif;
 }
 .container {
     width: 1440px;
